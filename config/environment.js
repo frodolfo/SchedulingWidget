f25bf3f597fd1cdad6d10e9ab56dde8c8796dffc
Fred Rodolfo <fred.rodolfo@cubic.com>
2021-10-11 11:29:05 -0700

    Changed rootURL for prod

--- config/environment.js
+++ config/environment.js
@@ -71,7 +71,7 @@ module.exports = function (environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
     ENV.APP.rootElement = '#content';
-    ENV.rootURL = '/SchedulingWidget/';
+    ENV.rootURL = '/SchedulingWidget';
   }
 
   return ENV;
